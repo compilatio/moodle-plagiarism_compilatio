ec99fb4bcf96e65620d632a1891edc6e2960a90f
Dan Marsden <dan@danmarsden.com>
2013-03-12 10:47:03 +1300

    Update version.php
    bump to include change to default web services address in plugins db

--- version.php
+++ version.php
@@ -14,9 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$plugin->version =  2012071903;
+$plugin->version =  2012071904;
 $plugin->requires = 2012062500.00;
 $plugin->cron     = 300; // Only run every 5 minutes.
 $plugin->component = 'plagiarism_compilatio';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.1';
+$plugin->release   = '1.1.1';
