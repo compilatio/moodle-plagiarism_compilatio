d5c668523fe2c01c31069b7c1c6990986bb7481b
Compilatio.net <compilatio@users.noreply.github.com>
2021-03-12 16:42:47 +0100

    Update and rename getWaitingTime.php to get_waiting_time.php

--- ajax/get_waiting_time.php
+++ ajax/get_waiting_time.php
@@ -40,7 +40,6 @@ require_login();
 $compilatio = compilatio_get_compilatio_service(get_config('plagiarism_compilatio', 'apiconfigid'));
 $reswait = $compilatio->get_waiting_time();
 $maxanalysistime = 1800;
-$reswait->magister_queue = 4000;
 if ($reswait != false && $reswait->magister_queue + $reswait->magister_analysis_time > $maxanalysistime) {
 
     $waittime = new Stdclass();
