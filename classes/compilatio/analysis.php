a6dd00b141eccb3c038e43107adf0c6e6fbdcbe1
Kyoo-COmpi <patry.kylian@compilatio.net>
2024-12-18 15:15:37 +0100

    update deprecated function (key)

--- classes/compilatio/analysis.php
+++ classes/compilatio/analysis.php
@@ -85,7 +85,7 @@ class analysis {
             $cmpfile->status = 'error_not_found';
         }
 
-        $recipe = key($doc->analyses);
+        $recipe = array_key_first((array)$doc->analyses);
 
         if (isset($doc->analyses->$recipe->state)) {
             $state = $doc->analyses->$recipe->state;
