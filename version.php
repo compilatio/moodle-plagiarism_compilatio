e048f10c31fbaf8a2474cd6b9730abdc6d931ec2
nr-Compilatio <151014306+nr-Compilatio@users.noreply.github.com>
2026-04-27 12:28:42 +0200

    Update plugin version and release number

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 defined('MOODLE_INTERNAL') || die('Direct access to this script is forbidden.');
 
 /** @var stdClass $plugin */
-$plugin->version    = 2026040100;
+$plugin->version    = 2026042700;
 $plugin->requires   = 2022041900;
 $plugin->cron       = 300; // Only run every 5 minutes.
 $plugin->component  = 'plagiarism_compilatio';
 $plugin->maturity   = MATURITY_STABLE;
-$plugin->release    = '3.2.11';
+$plugin->release    = '3.3.1';
